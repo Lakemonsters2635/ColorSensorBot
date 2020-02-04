6a0203bebc481cec4d18a35cbefb1c0bc9c045f7
tylerdevans <devrim.evans@gmail.com>
2020-02-03 17:30:13 -0800

    updated to 2020 command structure
    updated to 2020 command structure

--- src/main/java/frc/robot/commands/VisionLightCommand.java
+++ src/main/java/frc/robot/commands/VisionLightCommand.java
@@ -1,49 +1,72 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
-import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.model.FMSInfo;
+import frc.robot.subsystems.ColorMatcher;
+import frc.robot.subsystems.ColorSpinner;
+import frc.robot.subsystems.Vision;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
- *
+ * An example command that uses an example subsystem.
  */
-public class VisionLightCommand extends Command {
+public class VisionLightCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Vision m_Vision;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public VisionLightCommand(Vision subsystem) {
+    m_Vision = subsystem;
+ 
+    // Use addRequirements() here to declare subsystem dependencies.
+    //addRequirements(subsystem);
+  }
 
-    public VisionLightCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {   
+    m_Vision.ledOn(); 
+    System.out.println("vision light command initalized");
+  }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	RobotContainer.vision.ledOn();
-    }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //System.out.println("Vision light cmd execute");
+   m_Vision.data();
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        //System.out.println("Vision light cmd execute");
-        RobotContainer.vision.data();
+    m_Vision.printArea();
+  }
 
-        RobotContainer.vision.printArea();
-        
-    }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_Vision.ledOff();
+  }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-        //Robot.vision.driveMode();
-        RobotContainer.vision.ledOff();
-    }
-
-    // Called when another command which requires one or more of the same
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;                     
+  }
+ // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
+    	end(true);
     }
+
+  
+
 }
